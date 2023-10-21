f615fa566d146b268a3a6fa73ac3d6f8bfb1b4c7
dani <danielasanchezo2000@gmail.com>
2023-10-20 23:55:10 -0500

    adicion imagenes y precios

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import piedraFilosofal from '../Images/piedra_filosofal.png';
+import camarasecreta from '../Images/camara_secreta.png';
+import prisionero from '../Images/prisionero.png';
+import caliz from '../Images/caliz_fuego.png';
+import ordenFenix from '../Images/orden_fenix.png';
+import misterio from '../Images/misterio_principe.png';
+import reliquias from '../Images/reliquias_muerte.png';
+import legado from '../Images/legado_maldito.png';
 
 export const Books = () => {
   const [libros, setLibros] = useState([]);
@@ -18,26 +26,58 @@ export const Books = () => {
     // Puedes utilizar un estado para mantener un seguimiento de los libros en el carrito
   };
 
+  const price = [
+    25000,
+    20000,
+    30000,
+    35000,
+    15000,
+    40000,
+    32000,
+    28000
+  ];
+
+  const urlsImage = [
+    piedraFilosofal,
+    camarasecreta, 
+    prisionero, 
+    caliz,  
+    ordenFenix, 
+    misterio,
+    reliquias,
+    legado
+  ];
+
+  const resultBooks = libros.map((libro, i) => {
+    const priceResult = price[i];
+    const imageResult = urlsImage[i];
+    return { ...libro, priceResult, imageResult };
+  });
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {libros.map((libro) => (
-          <div key={libro.id} className="border p-4 shadow-md">
-            <div className="text-xl font-semibold">{libro.libro}</div>
-            <div className="text-blue-600 font-semibold mt-2">Fecha de lanzamiento: {libro.fecha_de_lanzamiento}</div>
-            <div className="text-xl font-semibold">{libro.descripcion}</div>
+        {resultBooks.map((books) => (
+          <div key={books.id} className="border p-4 shadow-md">
 
             <img
               className="w-40 h-40 mt-4"
-              src={libro.imagen}
-              alt={libro.title}
+              src={books.imageResult}
+              alt={books.titulo_original}
             />
+
+            <div className="text-xl font-semibold">{books.libro}</div>
+            <div className="text-blue-600 font-semibold mt-2">Fecha de lanzamiento: {books.fecha_de_lanzamiento}</div>
+            <div className="text-xl font-semibold">{books.descripcion}</div>
+            <div className="text-xl font-semibold">Precio: $ {books.priceResult}</div>
+
             <button
               className="bg-blue-500 text-white py-2 px-4 mt-4 hover:bg-blue-600"
-              onClick={() => handleCompra(libro)}
+              onClick={() => handleCompra(books)}
             >
               Comprar
             </button>
+            <br></br><br></br>
           </div>
         ))}
       </div>
