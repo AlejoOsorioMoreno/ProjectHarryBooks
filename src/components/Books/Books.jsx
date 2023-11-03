d4f4a97b476d3b76ea0f14e8eb7fbdb86aac8d5a
Alejandro <alejoosoriomoreno@gmail.com>
2023-11-02 19:05:50 -0500

    Adelanto de stock

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../Header/Header.jsx'
+import Header from '../Header/Header.jsx';
 import { price, stock, urlsImage } from '../../index.js';
 
 export const Books = () => {
@@ -7,6 +7,8 @@ export const Books = () => {
   const [cartCount, setCartCount] = useState(0);
   const [selectedLibros, setSelectedLibros] = useState([]);
   const [stockLeft, setStockLeft] = useState([...stock]);
+  const [selectedQuantities, setSelectedQuantities] = useState({}); // Nuevo estado para rastrear las cantidades
+
   const url = 'https://harry-potter-api.onrender.com/libros';
 
   useEffect(() => {
@@ -20,36 +22,56 @@ export const Books = () => {
 
   const handleCompra = (libro) => {
     const index = libros.findIndex((item) => item.id === libro.id);
-    if (index !== -1 && stockLeft[index] > 0) {
-      setSelectedLibros((prevSelectedLibros) => {
-        const updatedSelectedLibros = [...prevSelectedLibros];
-        const existingIndex = updatedSelectedLibros.findIndex((item) => item.id === libro.id);
+    const selectedQuantity = selectedQuantities[libro.id] || 1;
 
-        if (existingIndex !== -1) {
-          updatedSelectedLibros[existingIndex].quantity += 1;
-        } else {
-          updatedSelectedLibros.push({ ...libro, quantity: 1 });
-        }
+    if (index !== -1 && stockLeft[index] >= selectedQuantity) {
+      const updatedSelectedLibros = [...selectedLibros];
+      const existingIndex = updatedSelectedLibros.findIndex((item) => item.id === libro.id);
 
-        setStockLeft((prevStock) => {
-          const updatedStock = [...prevStock];
-          updatedStock[index] -= 1;
-          return updatedStock;
-        });
+      if (existingIndex !== -1) {
+        updatedSelectedLibros[existingIndex].quantity += selectedQuantity;
+      } else {
+        updatedSelectedLibros.push({ ...libro, quantity: selectedQuantity });
+      }
 
-        return updatedSelectedLibros;
+      setStockLeft((prevStock) => {
+        const updatedStock = [...prevStock];
+        updatedStock[index] -= selectedQuantity;
+        return updatedStock;
       });
 
-      setCartCount(cartCount + 1);
+      setSelectedLibros(updatedSelectedLibros);
+      updateCartCount(updatedSelectedLibros);
+      resetSelectedQuantity(libro); // Restablecer la cantidad después de la compra
     }
+  };
+
+  const updateCartCount = (selectedLibros) => {
+    let totalQuantity = 0;
+    for (const libro of selectedLibros) {
+      totalQuantity += libro.quantity;
+    }
+    setCartCount(totalQuantity);
+  };
+
+  const resetSelectedQuantity = (libro) => {
+    const id = libro.id;
+    setSelectedQuantities((prevQuantities) => {
+      const updatedQuantities = { ...prevQuantities };
+      updatedQuantities[id] = 1; // Restablecer la cantidad a 1
+      return updatedQuantities;
+    });
   };
 
   const resultBooks = libros.map((libro, i) => {
     const priceResult = price[i];
     const imageResult = urlsImage[i];
     const stockResult = stock[i];
-    
-    return { ...libro, priceResult, imageResult,stockResult };
+
+    const selectedLibro = selectedLibros.find((item) => item.id === libro.id);
+    const selectedQuantity = selectedQuantities[libro.id] || 1;
+
+    return { ...libro, priceResult, imageResult, stockResult, selectedQuantity };
   });
 
   return (
@@ -58,19 +80,28 @@ export const Books = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {resultBooks.map((books) => (
           <div key={books.id} className="border p-4 shadow-md">
-
-            <img
-              className="  h-60 w-60 ml-2"
-              src={books.imageResult}
-              alt={books.titulo_original}
-            />
-
+            <img className="h-60 w-60 ml-2" src={books.imageResult} alt={books.titulo_original} />
             <div className="text-xl font-semibold">{books.libro}</div>
             <div className="text-blue-600 font-semibold mt-2">Fecha de lanzamiento: {books.fecha_de_lanzamiento}</div>
             <div className="text-xl font-semibold">Precio: $ {books.priceResult}</div>
-            <div className="text-xl font-semibold">Stok:  {books.stockResult} Unidades</div>
+            <div className="text-xl font-semibold">Stock: {books.stockResult} Unidades</div>
+            <div className="text-xl font-semibold">Cantidad:
+              <input
+                type="number"
+                value={books.selectedQuantity}
+                min="1"
+                max={books.stockResult}
+                onChange={(e) => {
+                  const newQuantity = parseInt(e.target.value);
+                  setSelectedQuantities((prevQuantities) => ({
+                    ...prevQuantities,
+                    [books.id]: newQuantity,
+                  }));
+                }}
+              />
+            </div>
             <button
-              className="bg-blue-500 text-white py-2 px-4 mt-4 hover:bg-blue-600"
+              className="bg-blue-500 text-white py-2 px-4 mt-4 hover-bg-blue-600"
               onClick={() => handleCompra(books)}
             >
               Comprar
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,13 +12,17 @@ export default function Header({ cartCount, selectedLibros }) {
     setIsModalOpen(false);
   };
 
-  // Función para incrementar la cantidad de un libro en el carrito
-  const incrementQuantity = (libro) => {
+  // Función para agregar un libro al carrito
+  const addToCart = (libro) => {
     const updatedCart = [...cart];
-    const index = updatedCart.findIndex((item) => item.id === libro.id);
-    if (index !== -1) {
-      updatedCart[index].quantity += 1;
+    const existingItemIndex = updatedCart.findIndex((item) => item.id === libro.id);
+
+    if (existingItemIndex !== -1) {
+      updatedCart[existingItemIndex].quantity += 1;
+    } else {
+      updatedCart.push({ ...libro, quantity: 1 });
     }
+
     setCart(updatedCart);
   };
 
@@ -45,31 +49,46 @@ export default function Header({ cartCount, selectedLibros }) {
           <span className="ml-2 text-xl">{cartCount}</span>
         </div>
       </section>
-      {isModalOpen && (
-  <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-gray-900">
-    <div className="relative z-10 w-96 p-4 bg-white rounded shadow-lg">
-      <button className="absolute top-2 right-2 p-2 rounded-full bg-gray-200 hover:bg-gray-300 text-gray-600 hover:text-gray-800" onClick={closeCart}>
-        X
-      </button>
-      <div className="modal-content flex flex-col items-center">
-        <ul className="flex flex-col items-center space-y-4">
-          {selectedLibros.map((libro, i) => (
-            <div key={i}>
-              <li className="text-center">{libro.titulo_original}</li>
-              <li>
-                <img src={libro.imageResult} className="w-16" alt="" />
-              </li>
-              <li>{libro.priceResult} $</li>
-              <li>{libro.stockResult} Unidades</li>
-              <hr className="my-2 border-t border-gray-300" />
-            </div>
-          ))}
-        </ul>
-      </div>
-    </div>
-  </div>
-)}
 
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-gray-900 overflow-y-auto">
+          <div className="relative z-10 w-96 p-4 bg-white rounded shadow-lg">
+            <button
+              className="absolute top-2 right-2 p-2 rounded-full bg-gray-200 hover:bg-gray-300 text-gray-600 hover:text-gray-800"
+              onClick={closeCart}
+            >
+              X
+            </button>
+            <div className="modal-content flex flex-col items-center">
+              <ul className="flex flex-col items-center space-y-4">
+                {selectedLibros.map((libro, i) => (
+                  <div key={i}>
+                    <li className="text-center">{libro.titulo_original}</li>
+                    <li>
+                      <img src={libro.imageResult} className="w-16" alt="" />
+                    </li>
+                    <li>{libro.priceResult} $</li>
+                    <li>{libro.stockResult} Unidades</li>
+                    <li>
+                      Cantidad:
+                      <input
+                        type="number"
+                        min="1"
+                        value={libro.quantity}
+                        onChange={(e) => {
+                          const newQuantity = parseInt(e.target.value);
+                          addToCart({ ...libro, quantity: newQuantity });
+                        }}
+                      />
+                    </li>
+                    <hr className="my-2 border-t border-gray-300" />
+                  </div>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
